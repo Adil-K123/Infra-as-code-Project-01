0db59e72d80752cb797cf18baba47bfdc62e4dab
adilk <adilk@ubuntu-main.myguest.virtualbox.org>
2024-06-01 15:45:20 +0530

    Automated EC2 creation,necessary security group and keypair allocation using Terraform

---
+++ main.tf
@@ -0,0 +1,54 @@
+resource "aws_instance" "app_server" {
+  ami           = "ami-04b70fa74e45c3917"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.Project01_SG.name]
+  key_name = "Project01KeyPair"
+  tags = {
+    Name = "AdilProject01Appserver"
+  }
+}
+
+resource "aws_security_group" "Project01_SG" {
+  name        = "security group using Terraform"
+  description = "security group using Terraform"
+  vpc_id      = "vpc-0f87f7754e0c782b0"
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Project01_SG"
+  }
+}
